cfdb69e522e3ea0cc3c12539ede716462bf4936e
qiminghao <qiminghao@heyhomi.com>
2019-01-15 18:30:19 +0800

    添加druid监控

---
+++ util/src/main/java/com/food/util/DruidConfiguration.java
@@ -0,0 +1,100 @@
+package com.food.util;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2019/1/15 0015.
+ */
+@Configuration
+public class DruidConfiguration {
+
+    @Value(value = "${spring.datasource.url}")
+    private String dbUrl;
+    @Value(value = "${spring.datasource.username}")
+
+    private String username;
+    @Value(value = "${spring.datasource.password}")
+
+    private String password;
+    @Value(value = "${spring.datasource.driver-class-name}")
+
+    private String driverClassName;
+    @Value(value = "${spring.datasource.druid.initial-size}")
+    private int initialSize;
+    @Value(value = "${spring.datasource.druid.min-idle}")
+    private int minIdle;
+    @Value(value = "${spring.datasource.druid.max-active}")
+    private int maxActive = 50;
+    @Value(value = "${spring.datasource.druid.max-wait}")
+    private int maxWait;
+
+    /**
+     * 注册DruidServlet
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidServletRegistrationBean() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册DruidFilter拦截
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidFilterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<String, String>();
+        //设置忽略请求
+        initParams.put("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * 配置DataSource
+     *
+     * @return
+     * @throws SQLException
+     */
+    @Bean(initMethod = "init", destroyMethod = "close")
+    @Primary
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        druidDataSource.setUrl(dbUrl);
+        druidDataSource.setFilters("stat,wall");
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMinIdle(minIdle);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setMaxWait(maxWait);
+        druidDataSource.setUseGlobalDataSourceStat(true);
+        druidDataSource.setDriverClassName(driverClassName);
+        return druidDataSource;
+    }
+
+}
